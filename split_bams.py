24cddbfc41e5c465ced546ee8a2346331d091fb2
henryjuho <juhob@hotmail.co.uk>
2017-03-01 12:00:21 +0000

    script to split merged bams

---
+++ split_bams.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import argparse
+from qsub import *
+
+# arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-bam_dir', help='directory containing bam files', required=True)
+parser.add_argument('-evolgen', help='If specified will run on lab queue', default=False, action='store_true')
+args = parser.parse_args()
+
+# variables
+bam_dir = args.bam_dir
+bams = [bam_dir + x for x in os.listdir(bam_dir) if x.endswith('.bam')]
+evolgen = args.evolgen
+
+# split each bam
+for bam in bams:
+    split_cmd = "samtools split -f '%!.%*.bam' " + bam
+    q_sub([split_cmd], out=bam_dir + 'bam_split', evolgen=evolgen)
