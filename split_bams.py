d2de1d380561becc0a0a5320278a23f1ff33c57e
henryjuho <juhob@hotmail.co.uk>
2017-03-01 13:11:50 +0000

    corrected output directory

--- split_bams.py
+++ split_bams.py
@@ -11,12 +11,13 @@ args = parser.parse_args()
 
 # variables
 bam_dir = args.bam_dir
-bams = [bam_dir + x for x in os.listdir(bam_dir) if x.endswith('.bam')]
+bams = [x for x in os.listdir(bam_dir) if x.endswith('.bam')]
 evolgen = args.evolgen
 
 # split each bam
 counter = 0
 for bam in bams:
     counter += 1
+    cd_cmd = 'cd ' + bam_dir
     split_cmd = "samtools split -f '%!.%*.bam' " + bam
-    q_sub([split_cmd], out=bam_dir + 'bam_split_' + str(counter), evolgen=evolgen)
+    q_sub([cd_cmd, split_cmd], out=bam_dir + 'bam_split_' + str(counter), evolgen=evolgen)
