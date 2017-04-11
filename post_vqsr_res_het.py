f21829253cd3e5b73e26ef679cd4d3bdb4dd72e5
henryjuho <juhob@hotmail.co.uk>
2017-04-11 10:34:35 +0100

    corrected reading fromstdin

--- post_vqsr_res_het.py
+++ post_vqsr_res_het.py
@@ -4,7 +4,8 @@ import sys
 from qsub import *
 
 for vcf in sys.stdin:
+    vcf = vcf.rstrip('\n')
     dirname = vcf[:vcf.rfind('/')+1]
-    out = dirname + vcf.replace('.pass.vcf', '').split('_')[-1] + 'resid_het.txt'
+    out = dirname + vcf.replace('.pass.vcf', '').split('_')[-1] + '_resid_het.txt'
     cmd_line = 'cat ' + vcf + ' | python per_indiv_res_het.py > ' + out
     q_sub([cmd_line], out=out.replace('.txt', ''))
