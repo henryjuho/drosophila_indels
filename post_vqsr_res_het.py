c184ada660b1bc99ec28408a252895a42ce15fd3
henryjuho <juhob@hotmail.co.uk>
2017-04-11 10:49:50 +0100

    updated subprocess command

--- post_vqsr_res_het.py
+++ post_vqsr_res_het.py
@@ -7,5 +7,5 @@ for vcf in sys.stdin:
     vcf = vcf.rstrip('\n')
     dirname = vcf[:vcf.rfind('/')+1]
     out = dirname + vcf.replace('.pass.vcf', '').split('_')[-1] + '_resid_het.txt'
-    cmd_line = 'cat ' + vcf + ' | python per_indiv_res_het.py > ' + out
+    cmd_line = 'cat ' + vcf + ' | per_indiv_res_het.py > ' + out
     q_sub([cmd_line], out=out.replace('.txt', ''))
