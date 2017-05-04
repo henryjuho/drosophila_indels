175525138a3791102c91281dea274efb6f202970
henryjuho <juhob@hotmail.co.uk>
2017-05-04 13:22:06 +0100

    added evolgen flag

--- summary_sfs.py
+++ summary_sfs.py
@@ -9,6 +9,7 @@ parser.add_argument('-vcf', help='vcf file to summarise sfs for', required=True)
 parser.add_argument('-mode', help='mode to run in', choices=['SNP', 'INDEL'], required=True)
 parser.add_argument('-out', help='Output directory', required=True)
 parser.add_argument('-spp', help='Species', required=True)
+parser.add_argument('-evolgen', help='If specified will run on lab queue', action='store_true', default=False)
 args = parser.parse_args()
 
 # variables
@@ -19,6 +20,8 @@ mode = args.mode
 mode_dict = {'SNP': ['snp'], 'INDEL': ['ins', 'del']}
 region_dict = {'SNP': ['ALL', 'CDS_non_frameshift', 'intron', 'intergenic', 'intergenic_ar'],
                'INDEL': ['ALL', 'CDS_frameshift', 'CDS_non_frameshift', 'intron', 'intergenic', 'intergenic_ar']}
+evolgen = args.evolgen
+
 # SFS
 for m in mode_dict[mode]:
     for r in region_dict[mode]:
@@ -30,4 +33,4 @@ for m in mode_dict[mode]:
                    '-region {} '
                    '| sort | uniq -c | while read i; do echo " "$i; done | tr -s " " '
                    '> {}').format(vcf, m, r, new_out)
-        q_sub([sfs_cmd], out=new_out.replace('.txt', ''))
+        q_sub([sfs_cmd], out=new_out.replace('.txt', ''), evolgen=evolgen)
