4e1d893afc7439b2d060200f89b6fa340f62312b
henryjuho <juhob@hotmail.co.uk>
2017-07-12 16:18:51 +0100

    escape silly chars

--- per_gene_codeml.py
+++ per_gene_codeml.py
@@ -151,7 +151,7 @@ def main():
 
         counter += 1
         codeml_cmd = '/shared/evolgen1/shared_data/program_files/iceberg/codeml ' + \
-                     ctrl_file.replace('(', '\(').replace(')', '\)') + ' &'
+                     ctrl_file.replace('(', '\(').replace(')', '\)').replace("'", "\\'") + ' &'
         cmd_list.append(codeml_cmd)
 
     cmd_list += ['', 'wait']
