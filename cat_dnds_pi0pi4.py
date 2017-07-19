ad438fefa0dc3ffbddbda988b25470624c1a6f17
henryjuho <juhob@hotmail.co.uk>
2017-07-19 09:49:27 +0100

    now takes indel pi values also

--- cat_dnds_pi0pi4.py
+++ cat_dnds_pi0pi4.py
@@ -7,18 +7,21 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', help='File with dnds estimates', required=True)
 parser.add_argument('-p', help='File with pi0 pi4 estimates', required=True)
+parser.add_argument('-pI', help='File with pi estimates for INDELs', required=True)
 args = parser.parse_args()
 
 # variables
 pi_vals = {x.split()[0]: [x.split()[1], x.split()[2]] for x in open(args.p) if not x.startswith('trans_id')}
+indel_pi = {x.split()[0]: x.split()[1] for x in open(args.pI) if not x.startswith('trans_id')}
 
-print('trans', 'length', 'dN', 'dS', 'pi0', 'pi4', sep='\t')
+print('trans', 'length', 'dN', 'dS', 'pi0', 'pi4', 'pi_indel', sep='\t')
 for line in open(args.d):
     if not line.startswith('gene'):
         trans_id = line.split()[0]
         try:
             trans_pi = pi_vals[trans_id]
+            trans_indel_pi = indel_pi[trans_id]
         except KeyError:
             continue
-        out = line.rstrip() + '\t' + '\t'.join(trans_pi)
+        out = line.rstrip() + '\t' + '\t'.join(trans_pi) + '\t' + trans_indel_pi
         print(out)
