2801ba27396048e4fb18ce7e9503f7eeb8b23a25
henryjuho <juhob@hotmail.co.uk>
2017-08-03 12:53:35 +0100

    added class flag

--- anavar2ggplot.py
+++ anavar2ggplot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import argparse
 
 
 def reformat_line(line, header):
@@ -18,7 +19,7 @@ def reformat_line(line, header):
         else:
             data_to_sort[current_col] = current_val
 
-    # detrimine if ins class 1 or 2 is largest by ins gamma
+    # determine if ins class 1 or 2 is largest by ins gamma
     iga = data_to_sort['ins_gamma_1']
     igb = data_to_sort['ins_gamma_2']
 
@@ -29,7 +30,7 @@ def reformat_line(line, header):
         ic1 = '2'
         ic2 = '1'
 
-    # detrimine if del class 1 or 2 is largest by del gamma
+    # determine if del class 1 or 2 is largest by del gamma
     dga = data_to_sort['del_gamma_1']
     dgb = data_to_sort['del_gamma_2']
 
@@ -54,7 +55,40 @@ def reformat_line(line, header):
     return '\t'.join(new_header), out_lines
 
 
+def reformat_line_1class(line, header):
+
+    out_lines = []
+    new_header = ['run', 'imp', 'exit_code', 'theta', 'gamma', 'e', 'var_type', 'site_class', 'lnL', 'rep', 'region']
+    header = header.split()
+    out_data = {x: '' for x in new_header}
+    data_to_sort = {}
+    for i in range(0, len(header)):
+        current_col = header[i]
+        current_val = line.split()[i]
+        if 'ins' not in current_col and 'del' not in current_col:
+            out_data[current_col] = current_val
+        else:
+            data_to_sort[current_col] = current_val
+
+    # write multiple rows in long form
+    for site_class in [('ins', '1', '1'), ('del', '1', '1')]:
+        var_type = site_class[0]
+        out_data['theta'] = data_to_sort['{}_theta_{}'.format(var_type, site_class[1])]
+        out_data['gamma'] = data_to_sort['{}_gamma_{}'.format(var_type, site_class[1])]
+        out_data['e'] = data_to_sort['{}_e_{}'.format(var_type, site_class[1])]
+        out_data['var_type'] = var_type
+        out_data['site_class'] = site_class[2]
+
+        out_lines.append('\t'.join([out_data[y] for y in new_header]))
+
+    return '\t'.join(new_header), out_lines
+
+
 def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', help='number of classes', default=2, type=int)
+    args = parser.parse_args()
 
     header_line = ''
     data_line = 0
@@ -63,7 +97,10 @@ def main():
             header_line = line
         else:
             data_line += 1
-            new_lines = reformat_line(line, header_line)
+            if args.c == 2:
+                new_lines = reformat_line(line, header_line)
+            else:
+                new_lines = reformat_line_1class(line, header_line)
 
             if data_line == 1:
                 print new_lines[0]
