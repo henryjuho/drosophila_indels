9ea1381661536f244f57a8a87af82e6fea0271d9
henryjuho <juhob@hotmail.co.uk>
2017-08-22 10:12:56 +0100

    added sel v neu 2 class processing

--- anavar2ggplot.py
+++ anavar2ggplot.py
@@ -116,6 +116,59 @@ def reformat_line_selindel_1class(line, header):
     return '\t'.join(new_header), out_lines
 
 
+def reformat_line_selindel_2class(line, header):
+
+    out_lines = []
+    new_header = ['run', 'imp', 'exit_code', 'theta', 'gamma', 'e', 'var_type', 'site_class',
+                  'sel_type', 'lnL', 'rep', 'region']
+    header = header.split()
+    out_data = {x: '' for x in new_header}
+    data_to_sort = {}
+    for i in range(0, len(header)):
+        current_col = header[i]
+        current_val = line.split()[i]
+        if 'ins' not in current_col and 'del' not in current_col:
+            out_data[current_col] = current_val
+        else:
+            data_to_sort[current_col] = current_val
+
+    # determine if ins class 1 or 2 is largest by ins gamma
+    iga = data_to_sort['sel_ins_gamma_1']
+    igb = data_to_sort['sel_ins_gamma_2']
+
+    if float(iga) > float(igb):
+        ic1 = '1'
+        ic2 = '2'
+    else:
+        ic1 = '2'
+        ic2 = '1'
+
+    # determine if del class 1 or 2 is largest by del gamma
+    dga = data_to_sort['sel_del_gamma_1']
+    dgb = data_to_sort['sel_del_gamma_2']
+
+    if float(dga) > float(dgb):
+        dc1 = '1'
+        dc2 = '2'
+    else:
+        dc1 = '2'
+        dc2 = '1'
+
+    # write multiple rows in long form
+    for site_class in [('ins', ic1, '1'), ('ins', ic2, '2'), ('del', dc1, '1'), ('del', dc2, '2')]:
+        var_type = site_class[0]
+        out_data['theta'] = data_to_sort['sel_{}_theta_{}'.format(var_type, site_class[1])]
+        out_data['gamma'] = data_to_sort['sel_{}_gamma_{}'.format(var_type, site_class[1])]
+        out_data['e'] = data_to_sort['sel_{}_e_{}'.format(var_type, site_class[1])]
+        out_data['var_type'] = var_type
+        out_data['site_class'] = site_class[2]
+        out_data['sel_type'] = 'sel'
+
+        out_lines.append('\t'.join([out_data[y] for y in new_header]))
+
+    return '\t'.join(new_header), out_lines
+
+
 def reformat_line_selindel_continuous(line, header):
 
     out_lines = []
@@ -171,8 +224,10 @@ def main():
             else:
                 if args.c == 0:
                     new_lines = reformat_line_selindel_continuous(line, header_line)
-                else:
+                elif args.c == 1:
                     new_lines = reformat_line_selindel_1class(line, header_line)
+                else:
+                    new_lines = reformat_line_selindel_2class(line, header_line)
 
             if data_line == 1:
                 print new_lines[0]
