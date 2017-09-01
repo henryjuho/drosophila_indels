1e12b94df2f239208d54c5c45b719a5d7c59ff57
henryjuho <juhob@hotmail.co.uk>
2017-09-01 15:54:25 +0100

    added divergence table and reran divergence estimate

--- collate_indel_divergence.R
+++ collate_indel_divergence.R
@@ -10,14 +10,17 @@ non_coding_div$type = 'non-coding'
 div = rbind(cds_div, non_coding_div)
 
 all_chr = summarise(group_by(subset(div, chromo != 'X' & chromo != 'XHet' & chromo != 'YHet'), type),
-    indels=sum(indels), callable=sum(callable))
+    chromo='autosomes', indels=sum(indels), callable=sum(callable))
 
+all_chr$divergence = all_chr$indels / all_chr$callable
 
-all_chr$div = all_chr$indels / all_chr$callable
-
-div_plot = ggplot(all_chr, aes(x=type, y=div)) +
+div_plot = ggplot(all_chr, aes(x=type, y=divergence)) +
     geom_bar(stat='identity', position='dodge') +
     theme_bw(base_size = 10) +
     xlab('') + ylab('dvergence')
 
 ggsave('indel_divergence.pdf', plot=div_plot, width=3, height=3)
+
+out_csv = rbind(div, all_chr)
+
+write.csv(out_csv, file='indel_divergence.csv', row.names=FALSE)
