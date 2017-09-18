80d9688ee40ab7bf857b57b5fa0c5397372fadb4
henryjuho <juhob@hotmail.co.uk>
2017-09-18 14:31:08 +0100

    now calcs delta aic as well

--- calcAIC.py
+++ calcAIC.py
@@ -19,6 +19,19 @@ def aic(n_param, max_lnl):
     return (2 * k) - (2 * lnl)
 
 
+def delta_aic(current_d, best_d):
+
+    """
+    returns delta_AIC, which is defined as the difference between the AIC for the best fitting model
+    and the model on the row of the spreadsheet - from Kai's email 14th Sept 17
+    :param current_d: float
+    :param best_d: float
+    :return:
+    """
+
+    return best_d - current_d
+
+
 def main():
     # r params + neutral params + sel indel params
     no_params = {'sel_v_neu_continuous': 15 + 3 + 8,
@@ -28,17 +41,29 @@ def main():
                  'sel_v_neu_2class_equal_t': 15 + 3 + 11,
                  'sel_v_neu_1class_equal_t': 15 + 3 + 5}
 
-    for line in sys.stdin:
-        if line.startswith('run'):
-            print(line.rstrip(), 'n_params', 'AIC', sep=',')
-        else:
-            split_line = line.rstrip().split(',')
-            model = split_line[14]
-            max_l = float(split_line[11])
-            n = no_params[model]
-            line_aic = aic(n, max_l)
+    contents = [line for line in sys.stdin]
 
-            print(line.rstrip(), n, line_aic, sep=',')
+    print(contents[0].rstrip(), 'n_params', 'AIC', 'deltaAIC', sep=',')
+
+    results = []
+    aics = []
+    for res in contents[1:]:
+        split_line = res.rstrip().split(',')
+        model = split_line[14]
+        max_l = float(split_line[11])
+        n = no_params[model]
+        line_aic = aic(n, max_l)
+        split_line += [n, line_aic]
+        results.append(split_line)
+        aics.append(line_aic)
+
+    best_aic = sorted(aics)[0]
+
+    for line in results:
+
+        d_aic = delta_aic(line[-1], best_aic)
+
+        print(','.join([str(x) for x in line]), d_aic, sep=',')
 
 if __name__ == '__main__':
     main()
