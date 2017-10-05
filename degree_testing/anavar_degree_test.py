6ff9cd9582f9a7620df62fa1d375b44f04693e49
henryjuho <juhob@hotmail.co.uk>
2017-10-05 12:07:07 +0100

    added reduced model

--- degree_testing/anavar_degree_test.py
+++ degree_testing/anavar_degree_test.py
@@ -11,14 +11,17 @@ callsites = '/fastdata/bop15hjb/drosophila_data/dmel_ref/dmel.callablesites.summ
 out_dir = '/fastdata/bop15hjb/drosophila_data/dmel/anavar/degree_variation/'
 
 for degree in [25, 50, 75, 100, 150, 200, 300]:
+    for model in ['full', 'equal_t']:
 
-    out = '{}dmel_cds_v_4fold_snps_continuous_dfe_degree{}'.format(out_dir, degree)
-    cmd = ('cds_vs_neutral_anavar_snps.py '
-           '-vcf {} '
-           '-n 17 -c 1 -dfe continuous '
-           '-call_csv {}  '
-           '-neu_type 4fold '
-           '-out_pre {} -degree {}'
-           '').format(vcf, callsites, out, degree)
+        out = '{}dmel_cds_v_4fold_snps_continuous_dfe_degree{}.{}'.format(out_dir, degree, model)
+        cmd = ('cds_vs_neutral_anavar_snps.py '
+               '-vcf {} '
+               '-n 17 -c 1 -dfe continuous '
+               '-call_csv {}  '
+               '-neu_type 4fold '
+               '-out_pre {} -degree {}'
+               '').format(vcf, callsites, out, degree)
+        if model == 'equal_t':
+            cmd += ' -constraint equal_mutation_rate'
 
-    q_sub([cmd], out=out, t=48)
+        q_sub([cmd], out=out, t=48)
