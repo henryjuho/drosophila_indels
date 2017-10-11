ea2c9f801a9a9342c31183c76cf5881d1f7ab41e
henryjuho <juhob@hotmail.co.uk>
2017-10-11 15:51:05 +0100

    restruct to handle snps too, and to split across cores

--- cds_vs_neutral_anavar.py
+++ cds_vs_neutral_anavar.py
@@ -3,10 +3,11 @@
 from __future__ import print_function
 import anavar_utils as an
 import argparse
-from qsub import *
-import sys
+from qsub import q_sub
 import random
 from collections import Counter
+from vcf2raw_sfs import vcf2sfs
+
 # import gzip
 # import pysam
 # from vcf2raw_sfs import vcf2sfs
@@ -105,7 +106,7 @@ def resample_replace(site_freqs):
 def sfs2counts(freq_list, n):
     pos_biallelic_freqs = [round(i / float(n), 3) for i in range(1, int(n))]
 
-    counted_sorted_sfs = sorted(Counter(freq_list).most_common(), key=lambda z: z[0])
+    counted_sorted_sfs = sorted(Counter([str(x) for x in freq_list]).most_common(), key=lambda z: z[0])
     sfs_freq_dict = {x[0]: x[1] for x in counted_sorted_sfs}
 
     counts = []
@@ -120,59 +121,125 @@ def sfs2counts(freq_list, n):
     return counts
 
 
-def indel_sel_v_neu_anavar(ins_sfs, ins_m, del_sfs, del_m, n_i_sfs, n_i_m, n_d_sfs, n_d_m, constraint,
-                           bootstrap, n, c, dfe, out_stem, search, degree):
+def prepare_indel_sfs(vcf, call, n):
+
+    # extract site frequencies
+    del_sfs = vcf2sfs(vcf_name=vcf, mode='del',
+                      auto_only=True, skip_hetero=True,
+                      regions=['CDS_frameshift', 'CDS_non_frameshift'])
+
+    n_d_sfs = vcf2sfs(vcf_name=vcf, mode='del',
+                      auto_only=True, skip_hetero=True,
+                      regions=['intergenic', 'intron'])
+
+    ins_sfs = vcf2sfs(vcf_name=vcf, mode='ins',
+                      auto_only=True, skip_hetero=True,
+                      regions=['CDS_frameshift', 'CDS_non_frameshift'])
+
+    n_i_sfs = vcf2sfs(vcf_name=vcf, mode='ins',
+                      auto_only=True, skip_hetero=True,
+                      regions=['intergenic', 'intron'])
+
+    # convert to correct format for anavar
+    sfs_i = sfs2counts(ins_sfs, n)
+    sfs_d = sfs2counts(del_sfs, n)
+    sfs_ni = sfs2counts(n_i_sfs, n)
+    sfs_nd = sfs2counts(n_d_sfs, n)
+
+    # get callable sites
+    sel_m = call['ALL']['CDS']['pol']
+    neu_m = call['ALL']['intergenic']['pol'] + call['ALL']['intron']['pol']
+
+    # construct control file sfs
+    sfs_m = {'selected_INS': (sfs_i, sel_m), 'selected_DEL': (sfs_d, sel_m),
+             'neutral_INS': (sfs_ni, neu_m), 'neutral_DEL': (sfs_nd, neu_m)}
+
+    return sfs_m
+
+
+def prepare_snp_sfs(vcf, call, n):
+
+    # extract site frequencies
+    sel_sfs = vcf2sfs(vcf_name=vcf, mode='snp',
+                      auto_only=True, skip_hetero=True,
+                      regions=['CDS_frameshift', 'CDS_non_frameshift'])
+
+    neu_sfs = vcf2sfs(vcf_name=vcf, mode='snp',
+                      auto_only=True, skip_hetero=True,
+                      degen=4)
+
+    # convert to correct format for anavar
+    sfs_sel = sfs2counts(sel_sfs, n)
+    sfs_neu = sfs2counts(neu_sfs, n)
+
+    # get callable sites
+    sel_m = call['ALL']['CDS']['pol']
+    neu_m = call['ALL']['fourfold']['pol']
+
+    # construct control file sfs
+    sfs_m = {'selected_SNP': (sfs_sel, sel_m), 'neutral_SNP': (sfs_neu, neu_m)}
+
+    return sfs_m
+
+
+def sel_v_neu_anavar(mode, vcf, call, constraint, n, c, dfe, out_stem, search, degree, spread, evolgen):
 
     anavar_path = '/shared/evolgen1/shared_data/program_files/sharc/'
 
-    anavar_cmd = '{path}anavar1.22 {ctl} {rslts} {log}'
+    anavar_cmd = '{path}anavar1.22 {ctl} {rslts} {log} {seed}'
 
-    for i in [0] + range(1, bootstrap+1):
+    # sort file names
+    ctl_name = out_stem + '.control.txt'
 
-        # sort sfs
-        if i == 0:
-            sfs_i = ins_sfs
-            sfs_d = del_sfs
-            sfs_ni = n_i_sfs
-            sfs_nd = n_d_sfs
-        else:
-            sfs_i = resample_replace(ins_sfs)
-            sfs_d = resample_replace(del_sfs)
-            sfs_ni = resample_replace(n_i_sfs)
-            sfs_nd = resample_replace(n_d_sfs)
+    # make control file
+    if mode == 'snp':
+        sfs_data = prepare_snp_sfs(vcf, call, n)
+        ctl = an.SNPNeuSelControlFile()
 
-        # convert to correct format for anavar
-        sfs_i = sfs2counts(sfs_i, n)
-        sfs_d = sfs2counts(sfs_d, n)
-        sfs_ni = sfs2counts(sfs_ni, n)
-        sfs_nd = sfs2counts(sfs_nd, n)
-
-        # sort file names
-        ctl_name = out_stem + '.rep{}.control.txt'.format(i)
-        result_name = out_stem + '.rep{}.results.txt'.format(i)
-        log_name = out_stem + '.rep{}.log.txt'.format(i)
-
-        # construct control file
-        sfs_m = {'selected_INS': (sfs_i, ins_m), 'selected_DEL': (sfs_d, del_m),
-                 'neutral_INS': (sfs_ni, n_i_m), 'neutral_DEL': (sfs_nd, n_d_m)}
+    else:
+        sfs_data = prepare_indel_sfs(vcf, call, n)
         ctl = an.IndelNeuSelControlFile()
-        ctl.set_alg_opts(search=search, alg='NLOPT_LD_SLSQP')
-        ctl.set_data(sfs_m, n, dfe=dfe, c=c, gamma_r=(-5e5, 1e3), theta_r=(1e-10, 0.1), r_r=(0.01, 100))
-        if degree != 50:
-            ctl.set_dfe_optional_opts(degree=degree, optional=True)
-        ctl.set_constraint(constraint)
-        ctl_contents = ctl.construct()
-        with open(ctl_name, 'w') as control:
-            control.write(ctl_contents)
 
-        # call anavar
-        rep_cmd = anavar_cmd.format(path=anavar_path, ctl=ctl_name, rslts=result_name, log=log_name)
-        subprocess.call(rep_cmd, shell=True)
+    ctl.set_alg_opts(search=search, alg='NLOPT_LD_SLSQP')
+    ctl.set_data(sfs_data, n, dfe=dfe, c=c, gamma_r=(-5e5, 1e3), theta_r=(1e-10, 0.1), r_r=(0.01, 100))
+    if degree != 50:
+        ctl.set_dfe_optional_opts(degree=degree, optional=True)
+    ctl.set_constraint(constraint)
+    ctl_contents = ctl.construct()
+    with open(ctl_name, 'w') as control:
+        control.write(ctl_contents)
+
+    res_file_list = out_stem + '.allres.list.txt'
+    hjids = []
+    with open(res_file_list, 'w') as res_list:
+
+        # split into requested jobs
+        for i in range(0, spread):
+
+            split_stem = '{}.split{}'.format(out_stem, i)
+
+            result_name = split_stem + '.results.txt'
+            log_name = split_stem + '.log.txt'
+
+            print(result_name, file=res_list)
+
+            # call anavar
+            rep_cmd = anavar_cmd.format(path=anavar_path, ctl=ctl_name, rslts=result_name, log=log_name, seed=i)
+
+            q_sub([rep_cmd], out=split_stem, jid=split_stem + '.sh', t=8, evolgen=evolgen)
+            hjids.append(split_stem + '.sh')
+
+    # hold job to merge outputs
+    merge_out = out_stem + '.merged.results.txt'
+    gather = 'cat {} | gather_searches.py {}'.format(res_file_list, merge_out)
+    q_sub([gather], out=out_stem + '.merge', hold=hjids, evolgen=evolgen)
 
 
 def main():
     # arguments
     parser = argparse.ArgumentParser()
+    parser.add_argument('-mode', help='variant type to run on', choices=['snp', 'indel'], required=True)
+    parser.add_argument('-call_csv', help='Callable sites summary file', required=True)
     parser.add_argument('-vcf', help='VCF file to extract site frequencies from', required=True)
     parser.add_argument('-n', help='Sample size', required=True)
     parser.add_argument('-c', help='Number of classes to run model with', required=True, type=int)
@@ -180,71 +247,28 @@ def main():
                         choices=['discrete', 'continuous'])
     parser.add_argument('-constraint', help='Constraint for model', choices=['none', 'equal_mutation_rate'],
                         default='none')
-    parser.add_argument('-n_search', help='Number of searches to conduct', default=500, type=int)
+    parser.add_argument('-n_search', help='Number of searches to conduct per job', default=500, type=int)
+    parser.add_argument('-split', help='Number of jobs to split runs across, each job will run the control file once'
+                                       'with a different seed given to anavar', default=1, type=int)
     parser.add_argument('-degree', help='changes degree setting in anavar', default=50, type=int)
-    parser.add_argument('-call_csv', help='Callable sites summary file', required=True)
-    parser.add_argument('-bootstrap', help='Number of bootstrap replicates', default=0, type=int)
     parser.add_argument('-out_pre', help='File path and prefix for output', required=True)
-    parser.add_argument('-sub', help='If specified will submit script to cluster', action='store_true', default=False)
     parser.add_argument('-evolgen', help='If specified will run on evolgen', default=False, action='store_true')
     args = parser.parse_args()
-
-    # submission loop
-    if args.sub is True:
-        command_line = [' '.join([x for x in sys.argv if x != '-sub' and x != '-evolgen'])]
-        q_sub(command_line, args.out_pre, evolgen=args.evolgen, t=48)
-        sys.exit(0)
 
     # variables
     call_site_dict = read_callable_csv(args.call_csv)
     out_pre = args.out_pre
 
-    # extract site frequencies
-    del_sfs_cmd = ('/home/bop15hjb/sfs_utils/vcf2raw_sfs.py '
-                   '-vcf {vcf} '
-                   '-mode del -auto_only -skip_hetero '
-                   '-region CDS_frameshift -region CDS_non_frameshift'
-                   ).format(vcf=args.vcf)
-
-    ins_sfs_cmd = ('/home/bop15hjb/sfs_utils/vcf2raw_sfs.py '
-                   '-vcf {vcf} '
-                   '-mode ins -auto_only -skip_hetero '
-                   '-region CDS_frameshift -region CDS_non_frameshift'
-                   ).format(vcf=args.vcf)
-
-    n_i_sfs_cmd = ('/home/bop15hjb/sfs_utils/vcf2raw_sfs.py '
-                   '-vcf {vcf} '
-                   '-mode ins -auto_only -skip_hetero '
-                   '-region intergenic -region intron'
-                   ).format(vcf=args.vcf)
-
-    n_d_sfs_cmd = ('/home/bop15hjb/sfs_utils/vcf2raw_sfs.py '
-                   '-vcf {vcf} '
-                   '-mode del -auto_only -skip_hetero '
-                   '-region intergenic -region intron'
-                   ).format(vcf=args.vcf)
-
-    del_sfs = subprocess.Popen(del_sfs_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0].split('\n')[:-1]
-    ins_sfs = subprocess.Popen(ins_sfs_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0].split('\n')[:-1]
-    n_d_sfs = subprocess.Popen(n_d_sfs_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0].split('\n')[:-1]
-    n_i_sfs = subprocess.Popen(n_i_sfs_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0].split('\n')[:-1]
-
-    # get callable sites
-    del_m = call_site_dict['ALL']['CDS']['pol']
-    ins_m = call_site_dict['ALL']['CDS']['pol']
-    neu_m = call_site_dict['ALL']['intergenic']['pol'] + call_site_dict['ALL']['intron']['pol']
-
     # construct process
-    indel_sel_v_neu_anavar(ins_sfs=ins_sfs, ins_m=ins_m,
-                           del_sfs=del_sfs, del_m=del_m,
-                           n_i_sfs=n_i_sfs, n_i_m=neu_m,
-                           n_d_sfs=n_d_sfs, n_d_m=neu_m,
-                           constraint=args.constraint,
-                           bootstrap=args.bootstrap,
-                           n=args.n, c=args.c, dfe=args.dfe,
-                           out_stem=out_pre,
-                           search=args.n_search,
-                           degree=args.degree)
+    sel_v_neu_anavar(mode=args.mode,
+                     vcf=args.vcf, call=call_site_dict,
+                     constraint=args.constraint,
+                     n=args.n, c=args.c, dfe=args.dfe,
+                     out_stem=out_pre,
+                     search=args.n_search,
+                     degree=args.degree,
+                     spread=args.split,
+                     evolgen=args.evolgen)
 
 if __name__ == '__main__':
     main()
