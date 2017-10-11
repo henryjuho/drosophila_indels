588924ea4dcc423d4bfb54047676302c5aa168c7
henryjuho <juhob@hotmail.co.uk>
2017-10-11 17:07:06 +0100

    address seed 0 and gsl errors

--- cds_vs_neutral_anavar.py
+++ cds_vs_neutral_anavar.py
@@ -200,7 +200,7 @@ def sel_v_neu_anavar(mode, vcf, call, constraint, n, c, dfe, out_stem, search, d
         sfs_data = prepare_indel_sfs(vcf, call, n)
         ctl = an.IndelNeuSelControlFile()
 
-    ctl.set_alg_opts(search=search, alg='NLOPT_LD_SLSQP')
+    ctl.set_alg_opts(search=search, alg='NLOPT_LD_SLSQP', key=3, epsabs=1e-20, epsrel=1e-9, rftol=1e-9, optional=True)
     ctl.set_data(sfs_data, n, dfe=dfe, c=c, gamma_r=(-5e5, 1e3), theta_r=(1e-10, 0.1), r_r=(0.01, 100))
     if degree != 50:
         ctl.set_dfe_optional_opts(degree=degree, optional=True)
@@ -214,7 +214,7 @@ def sel_v_neu_anavar(mode, vcf, call, constraint, n, c, dfe, out_stem, search, d
     with open(res_file_list, 'w') as res_list:
 
         # split into requested jobs
-        for i in range(0, spread):
+        for i in range(1, spread+1):
 
             split_stem = '{}.split{}'.format(out_stem, i)
 
