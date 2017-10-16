c89dce13f5e94470f27458aa77583c1f08dae85b
henryjuho <juhob@hotmail.co.uk>
2017-10-16 12:51:44 +0100

    upped scale upper limit

--- cds_vs_neutral_anavar.py
+++ cds_vs_neutral_anavar.py
@@ -258,7 +258,8 @@ def sel_v_neu_anavar(mode, vcf, call, constraint, n, c, dfe, out_stem, search, d
                      epsabs=1e-20, epsrel=1e-9, rftol=1e-9,
                      maxtime=3600, optional=True)
 
-    ctl.set_data(sfs_data, n, dfe=dfe, c=c, gamma_r=(-5e4, 1e2), theta_r=(1e-10, 0.1), r_r=(0.01, 100))
+    ctl.set_data(sfs_data, n, dfe=dfe, c=c, gamma_r=(-5e4, 1e2), theta_r=(1e-10, 0.1), r_r=(0.01, 100),
+                 scale_r=(0.1, 5000.0))
     if degree != 50:
         ctl.set_dfe_optional_opts(degree=degree, optional=True)
     ctl.set_constraint(constraint)
