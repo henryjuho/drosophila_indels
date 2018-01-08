25d16b52c3a9335fb3ea49310aa2f31929dfb0e1
Henry Barton <hbarton2@sheffield.ac.uk>
2018-01-08 14:55:47 +0000

    indel sfs

---
+++ indel_sfs/dmel_ins_intergenic.sfs_job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source ~/.bash_profile
+
+#$-l h_rt=8:00:00
+#$-l mem=6G
+#$-l rmem=2G
+
+#$-P evolgen
+#$-q evolgen.q
+
+#$-N dmel_ins_intergenic.sfs_job.sh
+#$-o indel_sfs/dmel_ins_intergenic.sfs.out
+#$-e indel_sfs/dmel_ins_intergenic.sfs.error
+
+#$-V
+
+~/sfs_utils/vcf2raw_sfs.py -vcf /fastdata/bop15hjb/drosophila_data/dmel/analysis_ready_data/dmel_17flys.gatk.raw.indels.recalibrated.filtered_t95.0.pass.dpfiltered.50bp_max.bial.rmarked.polarised.annotated.ar.vcf.gz -mode ins -auto_only -skip_hetero -region intergenic | sort | uniq -c | while read i; do echo " "$i; done | tr -s " " > indel_sfs/dmel_ins_intergenic.sfs.txt
