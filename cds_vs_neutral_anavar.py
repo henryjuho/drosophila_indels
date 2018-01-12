915fabb9a8c856bd0cede5a1a8df56045d900d11
henryjuho <juhob@hotmail.co.uk>
2018-01-12 17:15:30 +0000

    added control over nnoimp and maximp

--- cds_vs_neutral_anavar.py
+++ cds_vs_neutral_anavar.py
@@ -4,7 +4,6 @@ from __future__ import print_function
 import anavar_utils as an
 import argparse
 from qsub import q_sub
-import random
 from collections import Counter
 from vcf2raw_sfs import vcf2sfs
 import random
@@ -220,7 +219,8 @@ def prepare_snp_sfs(vcf, call, n):
     return sfs_m
 
 
-def sel_v_neu_anavar(mode, vcf, call, constraint, n, c, dfe, alg, out_stem, search, degree, spread, evolgen):
+def sel_v_neu_anavar(mode, vcf, call, constraint, n, c, dfe, alg, nnoimp, maximp,
+                     out_stem, search, degree, spread, evolgen):
 
     """
     submits anavar jobs to cluster after writing required files etc
@@ -232,6 +232,8 @@ def sel_v_neu_anavar(mode, vcf, call, constraint, n, c, dfe, alg, out_stem, sear
     :param c: int
     :param dfe: str
     :param alg: str
+    :param nnoimp: int
+    :param maximp: int
     :param out_stem: str
     :param search: int
     :param degree: int
@@ -258,7 +260,8 @@ def sel_v_neu_anavar(mode, vcf, call, constraint, n, c, dfe, alg, out_stem, sear
 
     ctl.set_alg_opts(search=search, alg=alg, key=3,
                      epsabs=1e-20, epsrel=1e-9, rftol=1e-9,
-                     maxtime=3600, optional=True)
+                     maxtime=3600, optional=True,
+                     maximp=maximp, nnoimp=nnoimp)
 
     ctl.set_data(sfs_data, n, dfe=dfe, c=c, gamma_r=(-5e4, 1e2), theta_r=(1e-10, 0.1), r_r=(0.01, 100),
                  scale_r=(0.1, 5000.0))
@@ -312,6 +315,8 @@ def main():
     parser.add_argument('-n_search', help='Number of searches to conduct per job', default=500, type=int)
     parser.add_argument('-alg', help='Algorithm to use', default='NLOPT_LD_SLSQP',
                         choices=['NLOPT_LD_SLSQP', 'NLOPT_LD_LBFGS', 'NLOPT_LN_NELDERMEAD'])
+    parser.add_argument('-nnoimp', help='nnoimp value', default=1)
+    parser.add_argument('-maximp', help='maximp value', default=3)
     parser.add_argument('-split', help='Number of jobs to split runs across, each job will run the control file once'
                                        'with a different seed given to anavar', default=1, type=int)
     parser.add_argument('-degree', help='changes degree setting in anavar', default=50, type=int)
@@ -329,6 +334,7 @@ def main():
                      constraint=args.constraint,
                      n=args.n, c=args.c, dfe=args.dfe,
                      alg=args.alg,
+                     nnoimp=args.nnoimp, maximp=args.maximp,
                      out_stem=out_pre,
                      search=args.n_search,
                      degree=args.degree,
