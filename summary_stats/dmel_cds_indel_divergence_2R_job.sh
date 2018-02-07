81ecdbf775df9c77d90620f172fc14a3e29396a0
Henry Barton <hbarton2@sheffield.ac.uk>
2018-02-07 13:09:37 +0000

    summary stats

---
+++ summary_stats/dmel_cds_indel_divergence_2R_job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source ~/.bash_profile
+
+#$-l h_rt=8:00:00
+#$-l mem=6G
+#$-l rmem=2G
+
+#$-P evolgen
+#$-q evolgen.q
+
+#$-N dmel_cds_indel_divergence_2R_job.sh
+#$-o /home/bop15hjb/drosophila_indels/summary_stats/dmel_cds_indel_divergence_2R.out
+#$-e /home/bop15hjb/drosophila_indels/summary_stats/dmel_cds_indel_divergence_2R.error
+
+#$-V
+
+./indel_divergence.py -wga /fastdata/bop15hjb/drosophila_data/wga/multiple_alignment/dmel.dsim.dyak.wga.bed.gz -bed /fastdata/bop15hjb/drosophila_data/dmel_ref/dmel_cds.bed -out /home/bop15hjb/drosophila_indels/summary_stats/dmel_cds_indel_divergence.txt -chromo 2R
---
+++ summary_stats/dmel_noncoding_indel_divergence_3R_job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source ~/.bash_profile
+
+#$-l h_rt=8:00:00
+#$-l mem=6G
+#$-l rmem=2G
+
+#$-P evolgen
+#$-q evolgen.q
+
+#$-N dmel_noncoding_indel_divergence_3R_job.sh
+#$-o /home/bop15hjb/drosophila_indels/summary_stats/dmel_noncoding_indel_divergence_3R.out
+#$-e /home/bop15hjb/drosophila_indels/summary_stats/dmel_noncoding_indel_divergence_3R.error
+
+#$-V
+
+./indel_divergence.py -wga /fastdata/bop15hjb/drosophila_data/wga/multiple_alignment/dmel.dsim.dyak.wga.bed.gz -bed /fastdata/bop15hjb/drosophila_data/dmel_ref/dmel_noncoding.bed -out /home/bop15hjb/drosophila_indels/summary_stats/dmel_noncoding_indel_divergence.txt -chromo 3R
