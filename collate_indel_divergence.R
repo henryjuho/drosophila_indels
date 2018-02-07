dfb76d6d78bf49e9143435ba980521b26f0e535e
henryjuho <juhob@hotmail.co.uk>
2018-02-07 18:28:19 +0000

    added ar

--- collate_indel_divergence.R
+++ collate_indel_divergence.R
@@ -3,11 +3,15 @@ library(dplyr)
 
 cds_div = read.delim('divergence_data/dmel_cds_indel_divergence.txt')
 non_coding_div = read.delim('divergence_data/dmel_noncoding_indel_divergence.txt')
+ar_div = read.delim('divergence_data/dmel_ar_indel_divergence.txt')
 
 cds_div$type = 'cds'
 non_coding_div$type = 'non-coding'
+ar_div$type = 'ar'
 
-div = rbind(cds_div, non_coding_div)
+div = rbind(cds_div, non_coding_div, ar_div)
+
+div$type = factor(div$type, levels=c('cds', 'non-coding', 'ar'))
 
 all_chr = summarise(group_by(subset(div, chromo != 'X' & chromo != 'XHet' & chromo != 'YHet'), type, indel_type),
     chromo='autosomes', indels=sum(n_variants), callable=sum(callable))
@@ -18,7 +22,7 @@ div_plot = ggplot(all_chr, aes(x=type, y=divergence, fill=indel_type)) +
     geom_bar(stat='identity', position='dodge') +
     theme_bw(base_size = 10) +
     xlab('') + ylab('dvergence') +
-    theme(legend.title=element_blank(), legend.position=c(0.2, 0.8))
+    theme(legend.title=element_blank(), legend.position=c(0.2, 0.8), legend.background=element_blank())
 
 ggsave('indel_divergence.pdf', plot=div_plot, width=3, height=3)
 
