6899de4a93050ede1ace3956d159a0023e4c135b
Henry Barton <hbarton2@sheffield.ac.uk>
2018-02-07 18:19:23 +0000

    ar divergence

---
+++ divergence_data/dmel_ar_indel_divergence_3RHet_job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source ~/.bash_profile
+
+#$-l h_rt=8:00:00
+#$-l mem=6G
+#$-l rmem=2G
+
+#$-P evolgen
+#$-q evolgen.q
+
+#$-N dmel_ar_indel_divergence_3RHet_job.sh
+#$-o /home/bop15hjb/drosophila_indels/divergence_data/dmel_ar_indel_divergence_3RHet.out
+#$-e /home/bop15hjb/drosophila_indels/divergence_data/dmel_ar_indel_divergence_3RHet.error
+
+#$-V
+
+./indel_divergence.py -wga /fastdata/bop15hjb/drosophila_data/wga/multiple_alignment/dmel.dsim.dyak.wga.bed.gz -bed /fastdata/bop15hjb/drosophila_data/wga/multiple_alignment/dmel_ancestral_repeats.wga.bed.gz -out /home/bop15hjb/drosophila_indels/divergence_data/dmel_ar_indel_divergence.txt -chromo 3RHet
