989cbd4970a3b30ac5f0dfae93856a9d00c121f7
Henry Barton <hbarton2@sheffield.ac.uk>
2018-02-14 12:11:56 +0000

    added line data

---
+++ divergence_data/dmel_ar_LINEs_indel_divergence_dmel_mitochondrion_genome_job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source ~/.bash_profile
+
+#$-l h_rt=8:00:00
+#$-l mem=6G
+#$-l rmem=2G
+
+#$-P evolgen
+#$-q evolgen.q
+
+#$-N dmel_ar_LINEs_indel_divergence_dmel_mitochondrion_genome_job.sh
+#$-o /home/bop15hjb/drosophila_indels/divergence_data/dmel_ar_LINEs_indel_divergence_dmel_mitochondrion_genome.out
+#$-e /home/bop15hjb/drosophila_indels/divergence_data/dmel_ar_LINEs_indel_divergence_dmel_mitochondrion_genome.error
+
+#$-V
+
+./indel_divergence.py -wga /fastdata/bop15hjb/drosophila_data/wga/multiple_alignment/dmel.dsim.dyak.wga.bed.gz -bed /fastdata/bop15hjb/drosophila_data/dmel_ref/dmel-r5.34.ancLINEs.bed.gz -out /home/bop15hjb/drosophila_indels/divergence_data/dmel_ar_LINEs_indel_divergence.txt -chromo dmel_mitochondrion_genome
