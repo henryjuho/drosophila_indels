0101e544fc1946bc6c22537c054fece7336c2cd2
henryjuho <juhob@hotmail.co.uk>
2018-04-05 12:17:29 +0100

    rm old alpha approach

--- make_alpha_table.py
+++
@@ -1,9 +0,0 @@
-from __future__ import print_function
-import sys
-
-cds_dat = {x.split()[1]: x.split()[0] for x in open(sys.argv[1])}
-noncode_dat = {x.split()[1]: x.split()[0] for x in open(sys.argv[2])}
-
-print('x', 'pN', 'pS', sep='\t')
-for x in sorted(cds_dat.keys()):
-    print(x, cds_dat[x], noncode_dat[x], sep='\t')
--- merge_alpha.py
+++
@@ -1,9 +0,0 @@
-from __future__ import print_function
-import sys
-
-del_dat = {x.split()[0]: x.split()[1:] for x in open(sys.argv[1]) if not x.startswith('x')}
-ins_dat = {x.split()[0]: x.split()[1:] for x in open(sys.argv[2]) if not x.startswith('x')}
-
-print('x', 'pN', 'pS', sep='\t')
-for x in sorted(del_dat.keys()):
-    print(x, int(del_dat[x][0]) + int(ins_dat[x][0]), int(del_dat[x][1]) + int(ins_dat[x][1]), sep='\t')
